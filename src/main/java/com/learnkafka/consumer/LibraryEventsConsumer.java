e2cc72193feb36271f51216c2055410c780d3b8f
dmitriy.lakhno <dimalahno@rambler.ru>
2023-02-13 17:29:37 +0500

    add service to consumer

--- src/main/java/com/learnkafka/consumer/LibraryEventsConsumer.java
+++ src/main/java/com/learnkafka/consumer/LibraryEventsConsumer.java
@@ -1,5 +1,8 @@
 package com.learnkafka.consumer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.learnkafka.service.LibraryEventsService;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -7,10 +10,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@AllArgsConstructor
 public class LibraryEventsConsumer {
 
+    private final LibraryEventsService service;
+
     @KafkaListener(topics = {"library-events"})
-    public void onMessage(ConsumerRecord<Integer, String> consumerRecord) {
+    public void onMessage(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
         log.info("ConsumerRecord : {}", consumerRecord);
+        service.processLibraryEvent(consumerRecord);
     }
 }
