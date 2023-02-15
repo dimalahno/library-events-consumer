ee1b0f669493c9d7613295353e5c2762f474156e
dmitriy.lakhno <dimalahno@rambler.ru>
2023-02-15 07:31:05 +0500

    add error handler to retry message

--- src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
+++ src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -1,5 +1,6 @@
 package com.learnkafka.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
@@ -8,10 +9,22 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
 
 @Configuration
 @EnableKafka
+@Slf4j
 public class LibraryEventsConsumerConfig {
+
+    public DefaultErrorHandler errorHandler() {
+        var fixedBackOff = new FixedBackOff(1000L, 2);
+        var defaultErrorHandler = new DefaultErrorHandler(fixedBackOff);
+        defaultErrorHandler.setRetryListeners(((record, ex, deliveryAttempt) -> {
+            log.info("Failed Record in Retry Listener Exception : {} deliveryAttempt {} ", ex.getMessage(), deliveryAttempt);
+        }));
+        return defaultErrorHandler;
+    }
 
     @Bean
     ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
@@ -20,7 +33,8 @@ public class LibraryEventsConsumerConfig {
         ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         configurer.configure(factory, kafkaConsumerFactory.getIfAvailable());
         factory.setConcurrency(3);
-        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        factory.setCommonErrorHandler(errorHandler());
+//        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
         return factory;
     }
 }
